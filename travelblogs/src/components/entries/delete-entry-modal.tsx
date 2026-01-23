97f8a2532463eb178a70e26572f5d19a9f0d1cff
Thomas Dreyer <thomas-dreyer@gmx.de>
2026-01-23 16:49:25 +0100

    Bugfix: delete story had hidden dialog

--- travelblogs/src/components/entries/delete-entry-modal.tsx
+++ travelblogs/src/components/entries/delete-entry-modal.tsx
@@ -75,7 +75,7 @@ const DeleteEntryModal = ({
       </button>
 
       {isOpen ? (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40 px-4">
+        <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-black/40 px-4">
           <div
             role="dialog"
             aria-modal="true"
